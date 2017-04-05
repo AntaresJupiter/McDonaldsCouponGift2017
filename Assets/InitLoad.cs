93aa1d70f37d610ad5af708cb953b1869539715b
AntaresJupiter <contact@antaresentertainment.co.uk>
2017-04-05 03:19:09 +0200

    -

---
+++ Assets/InitLoad.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitLoad : MonoBehaviour {
+
+
+	public GameObject load;
+    public Vungle_me vungle;
+    public GameObject Error;
+
+    void Start () 
+	{
+        ShowLoad();
+	}
+
+
+    public void ShowLoad()
+    {
+        load.SetActive(true);
+        StartCoroutine(rectum());
+
+    }
+
+    IEnumerator rectum()
+    {
+        yield return new WaitForSeconds(25);
+        vungle.Show();
+        yield return new WaitForSeconds(2);
+        Error.SetActive(true);
+    }
+
+
+    
+}
---
+++ Assets/anus.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anus : MonoBehaviour 
+{
+
+	public GameObject Error;
+	public GameObject Load;
+	public Vungle_me vungle;
+
+
+	// Use this for initialization
+	public void ShowError () 
+	{
+		Error.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	public void ShowLoad () 
+	{
+		Load.SetActive (true);
+		StartCoroutine (rectum ());
+
+	}
+
+	IEnumerator rectum()
+	{
+		yield return new WaitForSeconds (5);
+		vungle.Show ();
+		yield return new WaitForSeconds (2);
+		Error.SetActive (true);
+	}
+
+	public void ShowInstructions () 
+	{
+
+	}
+
+
+	public void Back () 
+	{
+	}
+}
